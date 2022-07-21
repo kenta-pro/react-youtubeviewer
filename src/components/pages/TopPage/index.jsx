d6c1ac468fd9c45c07234ac38e2d0680107d736e
kenta-pro <kenta.yuri1026@gmail.com>
2022-07-21 14:57:18 +0900

    toppage-components

--- src/components/pages/TopPage/index.jsx
+++ src/components/pages/TopPage/index.jsx
@@ -1,25 +1,119 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import React, { useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types";
+import VideosListTemplate from "~/components/templates/VideosListTemplate";
+import Header from "~/components/organisms/Header";
+import SearchForm from "~/components/organisms/SearchForm";
+import VideosList from "~/components/organisms/VideosList";
+import axios from "axios";
 
-const Root = styled.div`
-  background: #e5e5e5;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-`;
-
-const Title = styled.h1`
-  font-size: 30px;
-  font-weight: bold;
-  color: #ff3300;
-`;
-
-const TopPage = () => (
-  <Root>
-    <Title>This is Top page!</Title>
-    <Link to="/play">Player Page</Link>
-  </Root>
+export const TopPagePresenter = ({
+  search,
+  searchNext,
+  defaultKeyword,
+  videos,
+  loading,
+}) => (
+  <VideosListTemplate
+    headerContents={<Header />}
+    searchFormContents={
+      <SearchForm onSubmit={search} defaultValue={defaultKeyword} />
+    }
+    videosListContents={<VideosList videos={videos} loading={loading} />}
+    onScrollEnd={searchNext}
+  />
 );
 
-export default TopPage;
+TopPagePresenter.propTypes = {
+  search: PropTypes.func.isRequired,
+  searchNext: PropTypes.func.isRequired,
+  defaultKeyword: PropTypes.string,
+  videos: VideosList.propTypes.videos,
+  loading: PropTypes.bool,
+};
+
+TopPagePresenter.defaultProps = {
+  videos: null,
+  loading: false,
+  defaultKeyword: "",
+};
+
+const TopPageContainer = ({ api, presenter, defaultKeyword }) => {
+  const [videos, setVideos] = useState([]);
+  const [nextPageToken, setNextPageToken] = useState(null);
+  const [keyword, setKeyword] = useState(defaultKeyword);
+  const [loading, setLoading] = useState(false);
+  const cleanedUp = useRef(false);
+
+  /**
+   * ビデオの取得
+   * @param pageToken 続きを取得する場合は前回取得時のレスポンスに含まれるnextPageTokenを指定する
+   * @returns {Promise<void>}
+   */
+  const getVideos = async (pageToken) => {
+    setLoading(true);
+    const {
+      data: { items, nextPageToken: newNextPageToken },
+    } = await api.search(keyword, { pageToken });
+    if (cleanedUp.current) {
+      return;
+    }
+    let nextVideos;
+    if (pageToken) {
+      const itemsWithoutDuplicated = items.filter(
+        ({ id: itemId }) => !videos.find(({ id }) => id === itemId)
+      );
+      nextVideos = videos.concat(itemsWithoutDuplicated);
+    } else {
+      nextVideos = items;
+    }
+    setVideos(nextVideos);
+    setNextPageToken(newNextPageToken);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    setNextPageToken(undefined);
+    setVideos([]);
+    getVideos();
+  }, [keyword]);
+
+  useEffect(
+    () => () => {
+      cleanedUp.current = true;
+    },
+    []
+  );
+
+  return presenter({
+    search: setKeyword,
+    searchNext: () => {
+      if (loading || !nextPageToken) {
+        return;
+      }
+      getVideos(nextPageToken);
+    },
+    defaultKeyword,
+    videos,
+    loading,
+  });
+};
+
+TopPageContainer.propTypes = {
+  api: PropTypes.shape({
+    search: PropTypes.func,
+  }),
+  defaultKeyword: PropTypes.string,
+  presenter: PropTypes.func.isRequired,
+};
+
+TopPageContainer.defaultProps = {
+  api: {
+    search: (keyword, params) =>
+      axios.get(`/api/videos/search/${keyword}`, { params }),
+  },
+  defaultKeyword: "ねこ",
+};
+
+export default (props) => (
+  <TopPageContainer presenter={TopPagePresenter} {...props} />
+);
